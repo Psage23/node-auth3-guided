0be112e668d2fcd047bb1bb6938d72d33c9eb916
Psage23 <pamelasage3@gmail.com>
2020-02-18 22:18:45 -0700

    app can register and login user with valid token

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-
+const jwt = require('jsonwebtoken');
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
@@ -18,4 +18,34 @@ server.get('/', (req, res) => {
   res.send("It's alive!");
 });
 
+//option 1
+
+// server.get('/token', (req, res) => {
+//   const token = jwt.sign({
+//     token: 'here it is',
+//     exp: 1000 * 60 * 5
+//   }, 'secret')
+//   res.status(400).json(token)
+// })
+
+
+//option 2
+
+server.get('/token', (req, res) => {
+  const payload = {
+    subject: 'thisuser',
+    userid: 'psage',
+    favoriteChili: 'green'
+  }
+
+  const secret = 'iamNotTellingYou';
+
+  const options = {
+    expiresIn: '8h'
+  };
+
+  const token = jwt.sign(payload, secret, options);
+
+  res.json(token);
+})
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
 const Users = require('../users/users-model.js');
+const secrets = require('../config/secrets')
 
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
@@ -25,9 +26,11 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = genToken(user);
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
-        });
+          message: `Welcome ${user.username}`,
+          token: token
+        })
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
@@ -37,4 +40,17 @@ router.post('/login', (req, res) => {
     });
 });
 
+function genToken(user) {
+  const payload = {
+    userid: user.id,
+    username: user.username,
+    //add any other info needed to be returned
+  }
+
+  const options = {
+    expiresIn: '8h'
+  };
+  const token = jwt.sign(payload, secrets.jwtSecret, options)
+  return token;
+}
 module.exports = router;
